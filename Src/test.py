ec0325df4ca028648f716b4a3ac1fe1c918f9c53
NishanthRachakonda <nishanth9sharma@gmail.com>
2019-11-10 17:51:16 +0530

    modified main file

--- Src/test.py
+++ Src/test.py
@@ -1,5 +1,14 @@
 """ Module to differentiate geniune, forged and duplicate signatures. """
 import cv2
 
-img = cv2.imread('fly.png', 0)
-surf = cv2.SURF(400)
+
+def main():
+    """ Main Functon. """
+    dirpath = '../Dataset_4NSigComp2010/Dataset_4NSigComp2010/TrainingSet/Disguise/'
+    img = cv2.imread(dirpath + 'D023.png', cv2.IMREAD_GRAYSCALE)
+    surf = cv2.SURF(400)
+    print(surf)
+
+
+if __name__ == '__main__':
+    main()
