9c06d86bf3f0006caeecfab4f8bd0a5983166e5a
NishanthRachakonda <nishanth9sharma@gmail.com>
2019-11-11 22:29:49 +0530

    added surf

--- Src/signature.py
+++ Src/signature.py
@@ -1,13 +1,17 @@
 """ Module to differentiate geniune, forged and duplicate signatures. """
 import cv2
+# import numpy as np
 
 
 def main():
     """ Main Functon. """
     dirpath = '../Dataset_4NSigComp2010/Dataset_4NSigComp2010/TrainingSet/Disguise/'
     img = cv2.imread(dirpath + 'D023.png', cv2.IMREAD_GRAYSCALE)
-    surf = cv2.SURF(400)
-    print(surf)
+    surf = cv2.xfeatures2d.SURF_create()
+    keypoints, _ = surf.detectAndCompute(img, None)
+    img = cv2.drawKeypoints(img, keypoints, None)
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
