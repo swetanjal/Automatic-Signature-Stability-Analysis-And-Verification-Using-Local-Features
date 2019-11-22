19b3d167933cd0ee5d3f3bbcef265123c3880bac
NishanthRachakonda <nishanth9sharma@gmail.com>
2019-11-22 15:16:40 +0530

    added two metrics

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ import pickle
 import threading
 
 ########################################### Helper Functions ##########################################
+
+
 def display_images(image):
     cv2.imshow('Showing image', image)
     cv2.waitKey()
@@ -55,7 +57,7 @@ def createDB():
         distances = []
         des = surf_db[i]
         for keypoint in des:
-            min_dist = np.min(np.linalg.norm(tmp_db - keypoint, axis = 1))
+            min_dist = np.mean(np.linalg.norm(tmp_db - keypoint, axis=1))
             sum_dist += min_dist
             distances.append(min_dist)
         avg_dist = sum_dist / des.shape[0]
@@ -66,20 +68,31 @@ def createDB():
     return np.array(finalDB)
 
 
-def classify(test_img, theta):
+def classify(test_img, theta, neighbourhood):
     surf = cv2.xfeatures2d.SURF_create(hessianThreshold=1000, extended=1)
     kp, des = surf.detectAndCompute(test_img, None)
     matched_points = 0
+    matched_avg = 0
+    non_matched_avg = 0
     sum_dist = 0
 
     for keypoint in des:
-        min_dist = np.min(np.linalg.norm(DB - keypoint, axis = 1))
+        min_dist = np.mean(np.sort(np.linalg.norm(
+            DB - keypoint, axis=1))[:neighbourhood])
         sum_dist += min_dist
         if min_dist <= theta:
             matched_points += 1
-
-    avg_dist = sum_dist / des.shape[0]
-    return "Matched Points: " + str(matched_points) + " Avg Dist: " + str(avg_dist) 
+            matched_avg += min_dist
+    # avg_dist = sum_dist / des.shape[0]
+    non_matched_avg = (sum_dist - matched_avg) / \
+        (des.shape[0] - matched_points)
+    if matched_points != 0:
+        matched_avg = matched_avg / matched_points
+    else:
+        matched_avg = 1
+    # return "Matched Points: " + str(matched_points) + " Avg Dist: " + str(avg_dist)
+    # return matched_points, avg_dist
+    return matched_points, matched_avg, non_matched_avg
 
 # DB = createDB()
 # storeData(DB, 'database')
@@ -88,24 +101,78 @@ def classify(test_img, theta):
 DB = loadData('database')
 print(DB.shape)
 
+test_size = 10
+neighbourhood = 1
 
 """ Number of matched points for Genuine Signatures """
+
 print("Genuine Signatures")
-file_names = os.listdir('../TrainingSet/Genuine')
+genuine_match = []
+genuine_mavg_dist = []
+genuine_navg_dist = []
+
+file_names = os.listdir('../TrainingSet/Genuine')[:test_size]
 for img_name in file_names:
-    print(classify(cv2.imread('../TrainingSet/Genuine/' +
-                              img_name, cv2.IMREAD_GRAYSCALE), 0.09))
+    matched_points, mavg_dist, navg_dist = classify(cv2.imread('../TrainingSet/Genuine/' +
+                                                               img_name, cv2.IMREAD_GRAYSCALE), 0.09, neighbourhood)
+    print(matched_points, mavg_dist, navg_dist)
+    genuine_match.append(matched_points)
+    genuine_mavg_dist.append(mavg_dist)
+    genuine_navg_dist.append(navg_dist)
 
 """ Number of matched points for Simulated Signatures """
+
+simulated_match = []
+simulated_mavg_dist = []
+simulated_navg_dist = []
+
 print("Forged Signatures")
-file_names = os.listdir('../TrainingSet/Simulated')
+file_names = os.listdir('../TrainingSet/Simulated')[:test_size]
 for img_name in file_names:
-    print(classify(cv2.imread('../TrainingSet/Simulated/' +
-                              img_name, cv2.IMREAD_GRAYSCALE), 0.09))
+    matched_points, mavg_dist, navg_dist = classify(cv2.imread('../TrainingSet/Simulated/' +
+                                                               img_name, cv2.IMREAD_GRAYSCALE), 0.09, neighbourhood)
+    print(matched_points, mavg_dist, navg_dist)
+    simulated_match.append(matched_points)
+    simulated_mavg_dist.append(mavg_dist)
+    simulated_navg_dist.append(navg_dist)
+
 
 """ Number of matched points for Disguised Signatures """
+
+disguised_match = []
+disguised_mavg_dist = []
+disguised_navg_dist = []
+
 print("Disguised Signatures")
-file_names = os.listdir('../TrainingSet/Disguise')
+file_names = os.listdir('../TrainingSet/Disguise')[:test_size]
 for img_name in file_names:
-    print(classify(cv2.imread('../TrainingSet/Disguise/' +
-                              img_name, cv2.IMREAD_GRAYSCALE), 0.09))
+    matched_points, mavg_dist, navg_dist = classify(cv2.imread('../TrainingSet/Disguise/' +
+                                                               img_name, cv2.IMREAD_GRAYSCALE), 0.09, neighbourhood)
+    print(matched_points, mavg_dist, navg_dist)
+    disguised_match.append(matched_points)
+    disguised_mavg_dist.append(mavg_dist)
+    disguised_navg_dist.append(navg_dist)
+
+
+# Results
+
+plt.title("Matched Points ; Neighbourhood : " + str(neighbourhood))
+plt.plot(range(10), genuine_match,  label="Genuine", color='r')
+plt.plot(range(10), simulated_match,  label="Simulated", color='g')
+plt.plot(range(10), disguised_match,  label="Disguised", color='b')
+plt.legend()
+plt.show()
+
+plt.title("Matched Avg Distance ; Neighbourhood : " + str(neighbourhood))
+plt.plot(range(10), genuine_mavg_dist,  label="Genuine", color='r')
+plt.plot(range(10), simulated_mavg_dist,  label="Simulated", color='g')
+plt.plot(range(10), disguised_mavg_dist,  label="Disguised", color='b')
+plt.legend()
+plt.show()
+
+plt.title("Non Matched Avg Distance ; Neighbourhood : " + str(neighbourhood))
+plt.plot(range(10), genuine_navg_dist,  label="Genuine", color='r')
+plt.plot(range(10), simulated_navg_dist,  label="Simulated", color='g')
+plt.plot(range(10), disguised_navg_dist,  label="Disguised", color='b')
+plt.legend()
+plt.show()
