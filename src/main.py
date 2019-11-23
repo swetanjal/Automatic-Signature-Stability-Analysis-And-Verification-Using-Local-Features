717de36871c296b2fd95dce7ddb1747ea9d0664a
TD87 <31207116+TD87@users.noreply.github.com>
2019-11-23 21:32:26 +0530

    Modularized, threading, histograms, fixed bugs

--- src/main.py
+++ src/main.py
@@ -1,160 +1,91 @@
-import cv2
-import matplotlib.pyplot as plt
-import matplotlib
-import PIL
-import numpy as np
 import os
+import cv2
 import pickle
 import threading
+import numpy as np
+import matplotlib.pyplot as plt
+from classify import classify
+from createDB import createDB
+
 
 ########################################### Helper Functions ##########################################
-
-
 def display_images(image):
     cv2.imshow('Showing image', image)
     cv2.waitKey()
 
-
 def storeData(dat, filename):
-    dbfile = open(filename, 'ab')
+    dbfile = open(filename, 'wb')
     pickle.dump(dat, dbfile)
     dbfile.close()
-
 
 def loadData(filename):
     dbfile = open(filename, 'rb')
     return pickle.load(dbfile)
+
+def gen_score(sig_type, test_size = None):
+    # Load File names
+    file_names = os.listdir('../TrainingSet/' + sig_type)
+    # np.random.shuffle(file_names)
+    if test_size is not None:
+        file_names = file_names[:test_size]
+
+    # Variables
+    idx = 0
+    limit = 20
+    threads = [None] * limit
+    avg = [None] * len(file_names)
+    match = [None] * len(file_names)
+
+    # Generate scores for each signature type
+    for i, img_name in enumerate(file_names):
+        # Run only limit number of threads
+        if idx % limit == 0 and idx != 0:
+            for j in range(limit):
+                threads[j].join()
+            idx = 0
+        # Run thread
+        threads[idx] = threading.Thread(target = classify, args = (cv2.imread('../TrainingSet/' + sig_type + '/' +
+                              img_name, cv2.IMREAD_GRAYSCALE), DB, threshold, neighbourhood, match, avg, i))
+        threads[idx].start()
+        idx += 1
+
+    for j in range(idx):
+        threads[j].join()
+    return match, avg
 #######################################################################################################
 
 
-def createDB():
-    # Load Images
-    img = []
-    file_names = os.listdir('../TrainingSet/Reference')
-    for img_name in file_names:
-        img.append(cv2.imread('../TrainingSet/Reference/' +
-                              img_name, cv2.IMREAD_GRAYSCALE))
-    img = np.array(img)
-
-    # Perform SURF for each Reference Image
-    surf_db = []
-    surf = cv2.xfeatures2d.SURF_create(hessianThreshold=1000, extended=1)
-    for i in range(img.shape[0]):
-        kp, des = surf.detectAndCompute(img[i], None)
-        surf_db.append(des)
-    surf_db = np.array(surf_db)
-
-    # Find stable keypoints
-    finalDB = []
-    for i in range(img.shape[0]):
-        # Generate LOO database
-        indexes = list(range(img.shape[0]))
-        indexes.remove(i)
-        tmp_db = surf_db[indexes].flatten()
-
-        # Find minimum distance for each keypoint
-        sum_dist = 0
-        distances = []
-        des = surf_db[i]
-        for keypoint in des:
-            min_dist = np.mean(np.linalg.norm(tmp_db - keypoint, axis=1))
-            sum_dist += min_dist
-            distances.append(min_dist)
-        avg_dist = sum_dist / des.shape[0]
-
-        # Append keypoints less than average distance
-        indexes = np.where(min_dist < avg_dist)
-        finalDB.extend(list(des[indexes]))
-    return np.array(finalDB)
-
-
-def classify(test_img, theta, neighbourhood):
-    surf = cv2.xfeatures2d.SURF_create(hessianThreshold=1000, extended=1)
-    kp, des = surf.detectAndCompute(test_img, None)
-    matched_points = 0
-    matched_avg = 0
-    non_matched_avg = 0
-    sum_dist = 0
-
-    for keypoint in des:
-        min_dist = np.mean(np.sort(np.linalg.norm(
-            DB - keypoint, axis=1))[:neighbourhood])
-        sum_dist += min_dist
-        if min_dist <= theta:
-            matched_points += 1
-            matched_avg += min_dist
-    # avg_dist = sum_dist / des.shape[0]
-    non_matched_avg = (sum_dist - matched_avg) / \
-        (des.shape[0] - matched_points)
-    if matched_points != 0:
-        matched_avg = matched_avg / matched_points
-    else:
-        matched_avg = 1
-    # return "Matched Points: " + str(matched_points) + " Avg Dist: " + str(avg_dist)
-    # return matched_points, avg_dist
-    return matched_points, matched_avg, non_matched_avg
-
 DB = createDB()
 storeData(DB, 'database')
-
-
 DB = loadData('database')
-
-test_size = 10
+# test_size = 10
+test_size = None
 neighbourhood = 1
-threshhold = 0.11
+threshold = 0.11
 
 
-""" Number of matched points for Genuine Signatures """
-
+# Matched Points for each type of signature
 print("Genuine Signatures")
-genuine_match = []
-genuine_mavg_dist = []
-genuine_navg_dist = []
+genuine_match, genuine_avg = gen_score('Genuine', test_size)
+print("Disguise Signatures")
+disguise_match, disguise_avg = gen_score('Disguise', test_size)
+print("Simulated Signatures")
+simulated_match, simulated_avg = gen_score('Simulated', test_size)
 
-file_names = os.listdir('../TrainingSet/Genuine')[:]
-for img_name in file_names:
-    matched_points, mavg_dist, navg_dist = classify(cv2.imread('../TrainingSet/Genuine/' +
-                                                                img_name, cv2.IMREAD_GRAYSCALE), threshhold, neighbourhood)
-    genuine_match.append(matched_points)
-    genuine_mavg_dist.append(mavg_dist)
-    genuine_navg_dist.append(navg_dist)
-
-""" Number of matched points for Simulated Signatures """
-
-simulated_match = []
-simulated_mavg_dist = []
-simulated_navg_dist = []
-
-print("Forged Signatures")
-file_names = os.listdir('../TrainingSet/Simulated')[:]
-for img_name in file_names:
-    matched_points, mavg_dist, navg_dist = classify(cv2.imread('../TrainingSet/Simulated/' +
-                                                                img_name, cv2.IMREAD_GRAYSCALE), threshhold, neighbourhood)
-    simulated_match.append(matched_points)
-    simulated_mavg_dist.append(mavg_dist)
-    simulated_navg_dist.append(navg_dist)
-
-""" Number of matched points for Disguised Signatures """
-
-disguised_match = []
-disguised_mavg_dist = []
-disguised_navg_dist = []
-
-print("Disguised Signatures")
-file_names = os.listdir('../TrainingSet/Disguise')[:]
-for img_name in file_names:
-    matched_points, mavg_dist, navg_dist = classify(cv2.imread('../TrainingSet/Disguise/' +
-                                                                img_name, cv2.IMREAD_GRAYSCALE), threshhold, neighbourhood)
-    disguised_match.append(matched_points)
-    disguised_mavg_dist.append(mavg_dist)
-    disguised_navg_dist.append(navg_dist)
 
 # Results
-plt.title("Matched Points ; Threshhold : " + str(threshhold))
-plt.scatter(genuine_match, [1 for i in range(len(genuine_match))], label="Genuine", color='r')
-plt.scatter(simulated_match, [2 for i in range(len(simulated_match))],  label="Simulated", color='g')
-plt.scatter(disguised_match, [3 for i in range(len(disguised_match))], label="Disguised", color='b')
-plt.savefig("matpoints" + str(threshhold) + ".png")
-plt.cla()
-plt.clf()
+plt.figure(figsize = (20, 8))
+plt.subplot(1, 2, 1)
+match = [genuine_match, disguise_match, simulated_match]
+plt.hist(match, label=['Genuine', 'Disguise', 'Simulated'])
+plt.legend()
+plt.title('Matched Points')
+
+plt.subplot(1, 2, 2)
+avg = [genuine_avg, disguise_avg, simulated_avg]
+plt.hist(avg, label=['Genuine', 'Disguise', 'Simulated'])
+plt.legend()
+plt.title('Average Distance')
+
+plt.savefig('Matchpoints' + str(threshold) + '.jpg')
+plt.show()
